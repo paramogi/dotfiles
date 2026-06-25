e54d2c348f5e77e7382ec44bba8cede397ec76d1
mogi <git@mogiya.dev>
2026-06-25 02:12:50 +0100

    fix bar hidden on unlock

--- dwl-v0.8/dwl.c
+++ dwl-v0.8/dwl.c
@@ -2603,7 +2603,10 @@ powermgrsetmode(struct wl_listener *listener, void *data)
 	wlr_output_commit_state(m->wlr_output, &state);
 
 	m->asleep = !event->mode;
-	updatemons(NULL, NULL);
+	if (event->mode) {
+		updatemons(NULL, NULL);
+		arrange(m);
+	}
 }
 
 void
