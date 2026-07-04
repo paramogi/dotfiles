87f5f19ee0cff042ed80367579989a1bb979e436
mogi <git@mogiya.dev>
2026-07-04 02:22:19 +0100

    update slstatus config

--- slstatus/config.h
+++ slstatus/config.h
@@ -69,9 +69,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ ram_perc, " %s%% | ", NULL },
-	{ temp, "%s°C ", "/sys/class/thermal/thermal_zone0/temp" },
-	{ cpu_perc, "%s%% | ", NULL },
+	{ ram_perc, "   %s%% | ", NULL },
+	{ temp, "  %s, ", "/sys/class/thermal/thermal_zone0/temp" },
+	{ cpu_perc, "  %s%% | ", NULL },
 	{ battery_icon, "%s%% | ", "BAT0" },
 	{ wifi_essid_icon, "%s | ", "wlan0" },
 	{ datetime, "%s ", "%b %d %H:%M" },
