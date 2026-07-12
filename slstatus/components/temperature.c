704e74b5260bf784f24e0e7057eafc4b75948699
mogi <git@mogiya.dev>
2026-07-12 20:00:25 +0100

    change slstatus config

--- slstatus/components/temperature.c
+++ slstatus/components/temperature.c
@@ -11,12 +11,20 @@
 	const char *
 	temp(const char *file)
 	{
-		uintmax_t temp;
+		uintmax_t temp, idx;
+		static const char *icons[5] = {
+			"", "", "", "", "",
+		};
 
 		if (pscanf(file, "%ju", &temp) != 1)
 			return NULL;
 
-		return bprintf("%ju", temp / 1000);
+		temp /= 1000;
+		idx = 1 + temp / 25;
+		if (idx > 4)
+			idx = 4;
+
+		return bprintf("%s %ju", icons[idx], temp);
 	}
 #elif defined(__OpenBSD__)
 	#include <stdio.h>
